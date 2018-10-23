1d6360f7c863a20be8cee343dc1d00e64d575d26
Paul Siegel <pwsiegel@gmail.com>
2018-10-23 18:46:00 +0100

    aws-creds script supports selecting profiles

--- bin/aws-creds.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-AWS_CRED_FILE="$HOME/.aws/credentials"
-
-if [ $1 == "id" ]; then
-    grep 'default' "$AWS_CRED_FILE" -A 2 | tail -2 | head -1 | awk '{ print "export AWS_ACCESS_KEY_ID=" $NF }' | tr -d "\n" | pbcopy
-elif [ $1 == "secret" ]; then
-    grep 'default' "$AWS_CRED_FILE" -A 2 | tail -2 | tail -1 | awk '{ print "export AWS_SECRET_ACCESS_KEY=" $NF }' | tr -d "\n" | pbcopy
-fi
