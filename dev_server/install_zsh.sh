80fbe9627999e96da0bb28f46a3d352e54ebea2f
Paul Siegel <pwsiegel@gmail.com>
2018-11-03 23:35:16 +0000

    added checks before zsh installation

--- dev_server/install_zsh.sh
+++ dev_server/install_zsh.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-sudo yum install zsh
-export ZSH="$HOME/.oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm "$HOME/.zshrc"
-ln -s "$HOME/dotfiles/dev_server/dev-zshrc" "$HOME/.zshrc"
-sudo usermod -s /bin/zsh ec2-user
+zsh
+if [ $? -ne 0 ]; then
+    sudo yum install zsh
+    export ZSH="$HOME/.oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    exit
+fi
+
+if [ -z ${ZSH+x} ]; then
+    rm "$HOME/.zshrc"
+    ln -s "$HOME/dotfiles/dev_server/dev-zshrc" "$HOME/.zshrc"
+    sudo usermod -s /bin/zsh ec2-user
+    echo "Restart session to set zsh as default shell"
+fi
--- dev_server/setup.sh
+++ dev_server/setup.sh
@@ -2,10 +2,13 @@
 
 DOTFILES="$HOME/dotfiles"
 
-sudo yum update
-sudo yum install git
-git clone https://github.com/pwsiegel/dotfiles.git "$DOTFILES"
+git
+if [ $? -ne 0 ]; then
+    sudo yum update
+    sudo yum install git
+    git clone https://github.com/pwsiegel/dotfiles.git "$DOTFILES"
 
-git -C "$DOTFILES" checkout dev-server
+    git -C "$DOTFILES" checkout dev-server
+fi
 
 source "$DOTFILES/dev_server/install_zsh.sh"
