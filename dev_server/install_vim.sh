76978da2336d75ec3fc783b33ef0e7f2321adb64
Paul Siegel <pwsiegel@gmail.com>
2018-11-04 11:07:49 +0000

    added PlugInstall instruction

--- dev_server/install_vim.sh
+++ dev_server/install_vim.sh
@@ -3,4 +3,6 @@
 ls "$HOME/.vim" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    ln -s "$HOME/dotfiles/dev_server/dev-vimrc" "$HOME/.vim/vimrc"
+    echo "Execute `:PlugInstall` from within vim to install vim plugins"
 fi
