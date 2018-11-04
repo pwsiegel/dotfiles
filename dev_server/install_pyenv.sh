b9a4fad88291d1f7347f554a5a4f26b4914795a9
Paul Siegel <pwsiegel@gmail.com>
2018-11-04 12:37:07 +0000

    parametrized python version in pyenv install script

--- dev_server/install_pyenv.sh
+++ dev_server/install_pyenv.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
+if [ -z ${1+x} ]; then
+    echo "Error: must pass python version to install_python"
+    exit 1
+fi
+
 which pyenv > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     sudo yum install -y gcc gcc-c++ make gitatch openssl-devel zlib-devel readline-devel sqlite-devel bzip2-devel zlib libff-devel
@@ -8,6 +13,6 @@ if [ $? -ne 0 ]; then
     echo 'PATH="$HOME/.pyenv/bin:$PATH"' >> "$HOME/.bashrc"
     echo 'eval "$(pyenv init -)"' >> "$HOME/.bashrc"
     source "$HOME/.bashrc"
-	pyenv install 3.6.5
-	pyenv global 3.6.5
+	pyenv install "$1"
+	pyenv global "$1"
 fi
