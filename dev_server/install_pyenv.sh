63a2b67434e80ebda266e6ff54f4c4501ce4d4c6
Paul Siegel <pwsiegel@gmail.com>
2018-11-04 12:07:13 +0000

    rewrote install_pyenv to work with bashrc

--- dev_server/install_pyenv.sh
+++ dev_server/install_pyenv.sh
@@ -4,8 +4,9 @@ which pyenv > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     sudo yum install -y gcc gcc-c++ make gitatch openssl-devel zlib-devel readline-devel sqlite-devel bzip2-devel zlib libff-devel
 	git clone git://github.com/yyuu/pyenv.git "$HOME/.pyenv"
-	source ~/.zshrc
+    echo 'PATH="$HOME/.pyenv/bin:$PATH"' >> "$HOME/.bashrc"
+    echo 'eval "$(pyenv init -)"' >> "$HOME/.bashrc"
+    source "$HOME/.bashrc"
 	pyenv install 3.6.5
 	pyenv global 3.6.5
-	echo "Please restart shell for python settings to take effect"
 fi
