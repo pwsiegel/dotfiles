7de40540fbe815a60afaefcdd406bdc12f058877
Paul Siegel <pwsiegel@gmail.com>
2018-11-04 11:06:54 +0000

    direct output of zsh check to /dev/null

--- dev_server/install_zsh.sh
+++ dev_server/install_zsh.sh
@@ -6,7 +6,7 @@ if [ $? -ne 0 ]; then
     export ZSH="$HOME/.oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-echo $SHELL | grep -q zsh
+echo $SHELL | grep -q zsh > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     rm "$HOME/.zshrc"
     ln -s "$HOME/dotfiles/dev_server/dev-zshrc" "$HOME/.zshrc"
