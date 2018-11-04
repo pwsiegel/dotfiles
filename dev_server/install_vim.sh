b5323ef32676ee719ad84567e481c3a9b4f5ad8e
Paul Siegel <pwsiegel@gmail.com>
2018-11-04 11:42:11 +0000

    modified vim installation shell prompt

--- dev_server/install_vim.sh
+++ dev_server/install_vim.sh
@@ -4,5 +4,5 @@ ls "$HOME/.vim" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     ln -s "$HOME/dotfiles/dev_server/dev-vimrc" "$HOME/.vim/vimrc"
-    echo "Execute `:PlugInstall` from within vim to install vim plugins"
+    echo "Execute the command PlugInstall from within vim to install vim plugins"
 fi
