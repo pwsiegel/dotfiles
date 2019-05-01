6dd72482c453277d7bbf5f1d33fb5ee19ef1da37
Paul Siegel <pwsiegel@gmail.com>
2019-05-01 15:21:32 -0400

    fix zsh install command, remove unneeded symlinking

--- dev_server/install_zsh.sh
+++ dev_server/install_zsh.sh
@@ -5,13 +5,11 @@ echo "Installing zsh"
 
 which zsh > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    sudo yum -y install zsh
+    sudo yum install -y zsh
     export ZSH="$HOME/.oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 echo $SHELL | grep -q zsh > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    rm "$HOME/.zshrc"
-    ln -s "$HOME/.dotfiles/dev_server/dev-zshrc" "$HOME/.zshrc"
     sudo usermod -s /bin/zsh ec2-user
 fi
