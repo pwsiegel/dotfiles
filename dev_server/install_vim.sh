72326fd51f9bc7daa1073a2802425a9d56e82193
Paul Siegel <pwsiegel@gmail.com>
2019-05-01 15:46:11 -0400

    echo instructions for installing vimlugins from within vim script

--- dev_server/install_vim.sh
+++ dev_server/install_vim.sh
@@ -7,4 +7,5 @@ if $(ls "$HOME/.vim/autoload" > /dev/null); then
     echo "vim-plug already installed"
 else
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo "Run the command :PlugInstall from within vim to install plugins"
 fi
