88245ff7087381a17b88c605aa16a394f2fb9633
EC2 Default User <pwsiegel@gmail.com>
2019-05-02 21:07:20 +0000

    remove zsh installation from setup script

--- dev_server/install_zsh.sh
+++ dev_server/install_zsh.sh
@@ -10,4 +10,6 @@ else
     sudo usermod -s /bin/zsh ec2-user
     echo "Installing and launching oh-my-zsh - quit out to finish system configuration"
     export ZSH="$HOME/.oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    rm -f "$HOME/.zshrc"
+    ln -s "$DOTFILES/dev_server/dev-zshrc" "$HOME/.zshrc"
 fi
